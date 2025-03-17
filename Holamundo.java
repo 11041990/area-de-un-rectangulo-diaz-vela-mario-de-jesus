8c7c883829c46d8b10637cb0a9a52a966c2e50e2
11041990 <vela3719diaz@gmail.com>
2025-03-17 13:40:12 -0600

    PRACTICA DE TEAMS

---
+++ Holamundo.java
@@ -0,0 +1,17 @@
+/
+package holamundo;
+
+/**
+ *
+ * @author mario
+ */
+public class Holamundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        system.out.println(¨Holamundo´);
+    }
+    
+}
